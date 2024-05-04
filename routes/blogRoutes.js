bbe47aba47c029c9996ec78b2d9579f9739d3be1
Juniper Watson <lyravox1@gmail.com>
2024-05-04 10:29:51 -0500

    Add blogRoutes.js

---
+++ routes/blogRoutes.js
@@ -0,0 +1,10 @@
+// routes/blogRoutes.js
+
+const express = require("express");
+const router = express.Router();
+
+router.get("/blog", (req, res) => {
+  res.render("blog");
+});
+
+module.exports = router;
